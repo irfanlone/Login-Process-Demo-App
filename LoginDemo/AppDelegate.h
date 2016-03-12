37aa76223f61875b1a4a8c28476e905e14ecd04b
Irfan <irfan.mf69@gmail.com>
2016-03-11 22:33:26 -0800

    Initial Commit

---
+++ LoginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginDemo
+//
+//  Created by Irfan Lone on 3/11/16.
+//  Copyright © 2016 Ilone Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginDemo
+//
+//  Created by Irfan Lone on 3/11/16.
+//  Copyright © 2016 Ilone Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
